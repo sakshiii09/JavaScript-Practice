231797bd9ff58320591360a0ac57dfa24eabce9f
Projects <mishrasakshi1106@gmail.com>
2024-09-11 12:42:08 +0530

    javascript values and variables

---
+++ variable.js
@@ -0,0 +1,32 @@
+//* ====================================
+//* Values and Variable in JavaScript
+//* ====================================
+
+//* In JavaScript, values and variables are fundamental concepts that form the basis of programming.
+
+//* Values:  A value is a piece of information that a program can work with. It can be a number, text, true/false, or more complex data.
+
+//* Variables: A variable is a container that holds a value. It has a name and can be used to store and manipulate data in a program.
+
+// console.log("welcome to best js course ");
+
+//! Let's test
+// var my_firstName = "John";
+// console.log(my_firstName);
+//? Explanation: This is a valid variable name. It starts with a letter, and the subsequent characters include letters, numbers, and an underscore. Follows JavaScript naming rules.
+
+// var _myLastName$ = "Doe";
+// console.log(_myLastName$);
+//? Explanation: This is a valid variable name. It starts with an underscore, and the subsequent characters include letters, numbers, and a dollar sign. Follows JavaScript naming rules.
+
+// var 123myAge = 25;
+// console.log(123myAge);
+//? Explanation: This is not a valid variable name. It starts with a number, which is not allowed as per JavaScript naming rules. Variable names cannot begin with a digit.
+
+// var $cityName = "New York";
+// console.log($cityName);
+//? Explanation: This is a valid variable name. It starts with a dollar sign, and the subsequent characters include letters. Follows JavaScript naming rules.
+
+// var my@Email = "john@example.com";
+// console.log(my@Email);
+//? Explanation: This is not a valid variable name. It includes the special character '@', which is not allowed in JavaScript variable names. Only letters, numbers, underscores, and dollar signs are allowed.
